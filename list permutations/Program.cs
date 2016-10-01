efa9592f58114d9f4d632fd42ddcb127b8412c4a
Jesse Serrao <jesseserrao@hotmail.co.uk>
2016-10-01 02:03:36 +0100

    Add program

---
+++ list permutations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_permutations
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter string");
+            evaluateString(Console.ReadLine());
+
+            Console.ReadLine();
+
+        }
+
+        static void evaluateString(string inputString)
+        {
+
+            Console.WriteLine(inputString);
+
+            if (inputString.Length > 1)
+            {
+                for(int i = 1; i < inputString.Length; i++)
+                {
+
+                    Console.WriteLine(inputString.Substring(0, i));
+
+                }
+
+                evaluateString(inputString.Substring(1));
+
+            }
+        }
+    }
+}
